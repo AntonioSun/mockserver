0d96b1643cc66243040c259b9cab562d49403537
Antonio Sun <asun@ats.l5.ca>
2022-01-31 13:41:49 -0500

    - [+] add Restriction level control

--- mockserver_opt.go
+++ mockserver_opt.go
@@ -12,6 +12,7 @@ Custom environment settings:
 - **MS_ADDR**: Server address (string="localhost:7070")
 - **MS_COMPRESS**: Enable transparent response compression (bool)
 - **MS_FILE**: Mock json file location (string="mock.json")
+- **MS_RESTRICT**: Restriction level (default: relaxed) (int)
 - **MS_VERBOSE**: Verbose mode (higher numbers increase the verbosity) (int="1")
 
 */
@@ -20,6 +21,7 @@ type envConfig struct {
 	Addr     string `env:"MS_ADDR" envDefault:"localhost:7070"` // Server address
 	Compress bool   `env:"MS_COMPRESS"`                         // Enable transparent response compression
 	File     string `env:"MS_FILE" envDefault:"mock.json"`      // Mock json file location
+	Restrict int    `env:"MS_RESTRICT"`                         // Restriction level (default: relaxed)
 	Verbose  int    `env:"MS_VERBOSE" envDefault:"1"`           // Verbose mode (higher numbers increase the verbosity)
 }
 
@@ -29,7 +31,7 @@ type envConfig struct {
 //  var (
 //          progname  = "mockserver"
 //          version   = "0.1.0"
-//          date = "2022-01-30"
+//          date = "2022-01-31"
 
 //          e envConfig
 //  )
