a40cecbe58bf8ff5c142aa86960052838c5b760d
Antonio Sun <asun@ats.l5.ca>
2022-02-01 12:21:14 -0500

    - [!] fix for: remove the restirction that only localhost allowed to connect

--- mockserver_opt.go
+++ mockserver_opt.go
@@ -9,7 +9,7 @@ package main
 
 Custom environment settings:
 
-- **MS_ADDR**: Server address (string="localhost:7070")
+- **MS_ADDR**: Server address (string=":7070")
 - **MS_COMPRESS**: Enable transparent response compression (bool)
 - **MS_FILE**: Mock json file location (string="mock.json")
 - **MS_RESTRICT**: Restriction level (default: relaxed) (int)
@@ -18,11 +18,11 @@ Custom environment settings:
 */
 
 type envConfig struct {
-	Addr     string `env:"MS_ADDR" envDefault:"localhost:7070"` // Server address
-	Compress bool   `env:"MS_COMPRESS"`                         // Enable transparent response compression
-	File     string `env:"MS_FILE" envDefault:"mock.json"`      // Mock json file location
-	Restrict int    `env:"MS_RESTRICT"`                         // Restriction level (default: relaxed)
-	Verbose  int    `env:"MS_VERBOSE" envDefault:"1"`           // Verbose mode (higher numbers increase the verbosity)
+	Addr     string `env:"MS_ADDR" envDefault:":7070"`     // Server address
+	Compress bool   `env:"MS_COMPRESS"`                    // Enable transparent response compression
+	File     string `env:"MS_FILE" envDefault:"mock.json"` // Mock json file location
+	Restrict int    `env:"MS_RESTRICT"`                    // Restriction level (default: relaxed)
+	Verbose  int    `env:"MS_VERBOSE" envDefault:"1"`      // Verbose mode (higher numbers increase the verbosity)
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -31,7 +31,7 @@ type envConfig struct {
 //  var (
 //          progname  = "mockserver"
 //          version   = "0.1.0"
-//          date = "2022-01-31"
+//          date = "2022-02-01"
 
 //          e envConfig
 //  )
